37b150a0e0011bc7f8cec28b3b9835e8bfc22331
John van Weel <john@johnvanweel.nl>
2019-03-09 16:10:13 +0100

    Application uses Swagger for communication

--- xbeereceiver/src/main/java/nl/johnvanweel/sensor/xbee/service/XbeeSampleListener.java
+++ xbeereceiver/src/main/java/nl/johnvanweel/sensor/xbee/service/XbeeSampleListener.java
@@ -4,6 +4,7 @@ import com.digi.xbee.api.RemoteXBeeDevice;
 import com.digi.xbee.api.io.IOLine;
 import com.digi.xbee.api.io.IOSample;
 import com.digi.xbee.api.listeners.IIOSampleReceiveListener;
+import nl.johnvanweel.iot.light.api.LightIntensityControllerApi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,25 +13,26 @@ import org.springframework.stereotype.Component;
 
 /**
  * Listener for Xbee serial connection packets.
- *
+ * <p>
  * Stores the received data on the network.
  */
 @Component
 public class XbeeSampleListener implements IIOSampleReceiveListener {
     private final Logger log = LoggerFactory.getLogger(XbeeSampleListener.class);
-    private final IlluminationMessageSenderService senderService;
+    private final LightIntensityControllerApi api;
 
     @Autowired
-    public XbeeSampleListener(IlluminationMessageSenderService senderService) {
-        this.senderService = senderService;
+    public XbeeSampleListener(final LightIntensityControllerApi api) {
+        this.api = api;
     }
 
     @Override
-	public void ioSampleReceived(RemoteXBeeDevice remoteXBeeDevice, IOSample ioSample) {
+    public void ioSampleReceived(RemoteXBeeDevice remoteXBeeDevice, IOSample ioSample) {
         long analogValue = ioSample.getAnalogValue(IOLine.DIO1_AD1);
-		double percentage = (analogValue) / (double) (1023);
-        double inverse = ((double)1)-percentage;
-        log.info("Sending new value {}", inverse);
-        senderService.sendMessage(inverse);
-	}
+        double percentage = (analogValue) / (double) (1023);
+        double inverse = ((double) 1) - percentage;
+        log.info("Sending new value {} to {}", inverse, api.getApiClient().getBasePath());
+
+        api.setLightsUsingPOST(inverse);
+    }
 }
