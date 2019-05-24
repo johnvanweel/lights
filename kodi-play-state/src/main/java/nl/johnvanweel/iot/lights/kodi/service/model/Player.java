de8cd381a589edd7768b8f9d881669dd872fbcb1
John van Weel <john@johnvanweel.nl>
2019-05-24 21:38:33 +0200

    Updated to later kodi

--- kodi-play-state/src/main/java/nl/johnvanweel/iot/lights/kodi/service/model/Player.java
+++ kodi-play-state/src/main/java/nl/johnvanweel/iot/lights/kodi/service/model/Player.java
@@ -3,6 +3,7 @@ package nl.johnvanweel.iot.lights.kodi.service.model;
 public class Player {
     private Integer playerid;
     private String type;
+    private String playertype;
 
     public Integer getPlayerid() {
         return playerid;
@@ -20,11 +21,20 @@ public class Player {
         this.type = type;
     }
 
+    public String getPlayertype() {
+        return playertype;
+    }
+
+    public void setPlayertype(String playertype) {
+        this.playertype = playertype;
+    }
+
     @Override
     public String toString() {
         return "Player{" +
-                "playerid='" + playerid + '\'' +
+                "playerid=" + playerid +
                 ", type='" + type + '\'' +
+                ", playertype='" + playertype + '\'' +
                 '}';
     }
 }
